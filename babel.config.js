457c639c0255c1cc0296345156388e92564fb090
Matt <mgould@breakingfreegroup.com>
2022-03-28 18:42:49 +0100

    optimisations

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+  presets: ['@vue/cli-plugin-babel/preset'],
+  exclude: ['./node_modules', './dist', './src/data/source']
 }
